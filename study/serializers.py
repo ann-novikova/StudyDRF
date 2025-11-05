943822d526e7f92e7851dd3b9df51bb6a4c530b8
Anna Novikova <anna.novikova.dev@gmail.com>
2025-11-05 18:06:55 +0300

    Docstrings, count lesson in serializer added

--- study/serializers.py
+++ study/serializers.py
@@ -1,15 +1,35 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from study.models import Course, Lesson
 
 
+class LessonSerializer(ModelSerializer):
+    """"Сериализатор для урока"""
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
 class CourseSerializer(ModelSerializer):
+    """
+    Сериализатор для модели Курса (Course).
+
+    Выводит:
+    основную информацию о курсе,
+    список всех уроков курса (вложенные данные),
+    общее количество уроков в курсе.
+    """
+
+    lessons_count = SerializerMethodField(read_only=True)
+
+    def get_lessons_count(self, obj):
+        """Возвращает количество уроков, связанных с курсом."""
+        return Lesson.objects.filter(course=obj).count()
+
     class Meta:
         model = Course
         fields = "__all__"
 
 
-class LessonSerializer(ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = "__all__"
+
