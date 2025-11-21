0753837ff7f16fbd59f24ef05441e3c6fd94f048
Anna Novikova <anna.novikova.dev@gmail.com>
2025-11-21 10:24:25 +0300

    Unused import deleted

--- users/views.py
+++ users/views.py
@@ -8,7 +8,6 @@ from rest_framework.generics import (CreateAPIView, DestroyAPIView,
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from study.models import Course
 from users.models import Payment, Subscription, User
