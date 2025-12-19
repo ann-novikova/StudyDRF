852c79e55d8afc3436b3075508ee53fae03e2d0b
Anna Novikova <anna.novikova.dev@gmail.com>
2025-12-19 15:54:15 +0300

    test ci

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from datetime import timedelta
 from pathlib import Path
 
@@ -182,3 +183,11 @@ LOGGING = {
         },
     },
 }
+
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "test_db.sqlite3",
+        }
+    }
